6f65287b5fd748367186f1145e3c959993512ab4
wanchao <471937306@qq.com>
2018-09-18 11:54:57 +0800

    build

--- config/url.js
+++ config/url.js
@@ -1,6 +1,6 @@
 import env from './env'
 
 const DEV_URL = 'http://localhost:8080/'
-const PRO_URL = 'https://littlebug.com'
+const PRO_URL = 'http://api.littlebug.com'
 
 export default env === 'development' ? DEV_URL : PRO_URL
