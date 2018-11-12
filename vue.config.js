f513e60e08fc211617c76cf036305480a0becf14
wanchao <471937306@qq.com>
2018-11-12 17:32:51 +0800

    https

--- vue.config.js
+++ vue.config.js
@@ -15,8 +15,8 @@ fs.writeFileSync(path.join(__dirname, './config/env.js'), `export default '${env
 // 如果您的应用程序部署在子路径中，则需要在这指定子路径
 // 例如：https://www.foobar.com/my-app/
 // 需要将它改为'/my-app/'
-const BASE_URL = env === 'production' ? 'http://littlebug.oss-cn-beijing.aliyuncs.com/admin.littlebug/' : '/'
-const target = env === 'production' ? 'http://api.littlebug.vip/' : 'http://localhost:8083/'
+const BASE_URL = env === 'production' ? 'https://littlebug.oss-cn-beijing.aliyuncs.com/admin.littlebug/' : '/'
+const target = env === 'production' ? 'https://api.littlebug.vip/' : 'http://localhost:8083/'
 // const target = 'http://api.littlebug.vip/'
 module.exports = {
   devServer: {
@@ -25,26 +25,9 @@ module.exports = {
         target: target, // 源地址
         changeOrigin: true // 改变源
       }
-      // '/cap_union_new_verify': {
-      //     target: 'https://ssl.captcha.qq.com', //源地址
-      //     changeOrigin: true, //改变源
-      //     // pathRewrite: {
-      //     //     '^/admin': "/" //路径重写
-      //     // }
-      // },
-      // '/caplog': {
-      //   target: 'https://dj.captcha.qq.com', //源地址
-      //   changeOrigin: true, //改变源
-      //   // pathRewrite: {
-      //   //     '^/admin': "/" //路径重写
-      //   // }
-      // }
     }
   },
   baseUrl: BASE_URL,
-  // baseUrl: BASE_URL,
-  // tweak internal webpack configuration.
-  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
