39e40def3670180975ef1a670063c23e460fd818
wanchao <471937306@qq.com>
2018-11-23 16:00:01 +0800

    oss and http

--- config/url.js
+++ config/url.js
@@ -1,3 +1,3 @@
 const DEV_URL = 'http://localhost:8080/'
-const PRO_URL = 'https://api.littlebug.vip/'
+const PRO_URL = 'http://api.littlebug.vip/'
 export default process.env.NODE_ENV === 'development' ? DEV_URL : PRO_URL
